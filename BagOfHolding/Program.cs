9e32692e1f4a47623c89ab4307d7499e72366394
The Dude <The Dude@THEDUDE>
2017-02-03 23:50:24 +0100

    Various notes and rewrites

--- BagOfHolding/Program.cs
+++ BagOfHolding/Program.cs
@@ -13,51 +13,59 @@ namespace BagOfHolding
             BagOfHolding Bag = new BagOfHolding();
             while (true)
             {
+                Console.Clear();
                 switch (Bag.options())
                 {
                     case 1:
-                        Bag.list();
+                        Bag.contents();
                         break;
                     case 2:
                         Bag.add();
                         break;
                     case 3:
-                        Bag.list();
+                        Bag.contents();
                         Bag.remove();
                         break;
                     default:
                         Console.WriteLine("What the hell are you trying to do?!");
                         break;
                 }
+                Console.ReadLine();
             }
         }
     }
     class BagOfHolding
     {
 
-        private string[] contents = new string[0];
+        private string[] contentsarray = new string[0];
 
 
 
-// Skriver menuen og returnerer et brugerindtastet heltal
-        public int options()
+// Skriver menuen 
+        public void menu()
         {
             Console.WriteLine("1. Look in the Bag of Holding  ");
             Console.WriteLine("2. Put an item into the Bag of Holding ");
             Console.WriteLine("3. Remove an item from the Bag of Holding  ");
+        }
+//Kalder menufunktion og returnerer et brugerindtastet heltal
+        public int options()
+        {
+            menu();
             return userinput();
         }
-        public void list()
+        public void contents()
         {
 
-            if (contents.Length < 1)
+            if (contentsarray.Length < 1)
             {
                 Console.WriteLine("The bag of holding is empty");
+                return;
             }
             else
             {
                 int cycle = 1;
-                foreach (string k in contents)
+                foreach (string k in contentsarray)
                 {
                     Console.WriteLine("{0}. {1}", cycle, k);
                     cycle++;
@@ -70,37 +78,42 @@ namespace BagOfHolding
         {
             Console.WriteLine("What would you like to add?");
             string stuff = Console.ReadLine();
-            string[] temp = new string[contents.Length + 1];
-            for (int i = 0; i < contents.Length; i++)
+            string[] temp = new string[contentsarray.Length + 1];
+            for (int i = 0; i < contentsarray.Length; i++)
             {
-                temp[i] = contents[i];
+                temp[i] = contentsarray[i];
             }
-            temp[contents.Length] = stuff;
-            contents = temp;
+            temp[contentsarray.Length] = stuff;
+            contentsarray = temp;
         }
         public void remove()
         {
-            if (contents.Length == 0)
+            if (contentsarray.Length == 0)
             {
                 Console.WriteLine("There is nothing to remove...");
                 return;
             }
             Console.WriteLine("Which item  would you like to remove?");
             int itemnumber = userinput() - 1;
-            string[] temp = new string[contents.Length - 1];
+            if (itemnumber > contentsarray.Length - 1)
+            {
+                Console.WriteLine("There is nothing there...");
+                return;
+            }
+            string[] temp = new string[contentsarray.Length - 1];
             int j = 0;
-            for (int i = 0; i < contents.Length; i++)
+            for (int i = 0; i < contentsarray.Length; i++)
             {
                 if (i != itemnumber)
                 {
-                    temp[j] = contents[i];
+                    temp[j] = contentsarray[i];
                     ++j;
                 }
             }
-            contents = temp;
+            contentsarray = temp;
 
         }
-
+//forsøger at konvertere brugerens input til int
         public int userinput()
         {
             int number;
@@ -111,7 +124,6 @@ namespace BagOfHolding
             }
             catch (Exception)
             {
-                Console.WriteLine("What the hell are you trying to do?!");
                 return 4;
             }
         }
